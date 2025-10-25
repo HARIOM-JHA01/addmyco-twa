400b9082194a12cb5abf8cd126d7f97a05578919
HARIOM-JHA01 <jhahariom040@gmail.com>
2025-10-25 12:18:50 +0530

    feat: Improve service worker management by unregistering existing service workers on app initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-import WebApp from '@twa-dev/sdk'
+import WebApp from "@twa-dev/sdk";
 
 WebApp.ready();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .getRegistrations()
+    .then((registrations) => {
+      registrations.forEach((registration) => {
+        registration.unregister().catch((err) => {
+          console.error("ServiceWorker unregister failed:", err);
+        });
+      });
+      console.info("Service workers unregistered (if any).");
+    })
+    .catch((err) => {
+      console.error("Failed to get service worker registrations:", err);
+    });
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
