5d886c9d6cc8ef90aaee0d50b3be2955b815e649
HARIOM-JHA01 <jhahariom040@gmail.com>
2025-10-25 12:21:13 +0530

    feat: Enhance Vite configuration with manual chunking for optimized build output

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,27 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "./dist",
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router-dom")
+            )
+              return "react-vendor";
+            if (id.includes("@fortawesome")) return "fontawesome";
+            if (id.includes("@twa-dev") || id.includes("qrcode.react"))
+              return "twa-qrcode";
+            if (id.includes("axios") || id.includes("zustand"))
+              return "vendor-helpers";
+            return "vendor";
+          }
+        },
+      },
+    },
   },
   base: "/",
   server: {
